ea32fa099563f0611c9b29bc16ae598efe170c1f
ElderSam <eldersamuel98@gmail.com>
2020-05-01 17:41:00 -0300

    configurando endereço do socket para conectar ao Backend (Deploy no Heroku)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # chat-app
+
+
+Aplicação de Chat em tempo real
+
+OBS: o front-end está na pasta public e todo o resto é o back-end. Fiz isso para colocar tudo em um mesmo repositório e ser reconhecido pelo Heroku quando inicia o node server na pasta raíz.
--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,7 @@
 
-var socket = io('http://localhost:3000'); //conecta com o Socket do backend, ouve essa conexão
-    
+var socket = io('https://chat-app2-node.herokuapp.com/'); //conecta com o Socket do backend, ouve essa conexão
+//var socket = io('http://localhost:3000');   
+
 var objDiv = document.querySelector(".messages");
 
 function renderMessage(message, type){
@@ -10,19 +11,11 @@ function renderMessage(message, type){
     var data = new Date();
 
     // Guarda cada pedaço em uma variável
-    var dia     = data.getDate();           // 1-31
-    var dia_sem = data.getDay();            // 0-6 (zero=domingo)
-    var mes     = data.getMonth();          // 0-11 (zero=janeiro)
-    var ano2    = data.getYear();           // 2 dígitos
-    var ano4    = data.getFullYear();       // 4 dígitos
     var hora    = data.getHours();          // 0-23
     var min     = data.getMinutes();        // 0-59
     var seg     = data.getSeconds();        // 0-59
-    var mseg    = data.getMilliseconds();   // 0-999
-    var tz      = data.getTimezoneOffset(); // em minutos
 
-    // Formata a data e a hora (note o mês + 1)
-    var str_data = dia + '/' + (mes+1) + '/' + ano4;
+    // Formata a hora
     var str_hora = hora + ':' + min + ':' + seg;
 
     if(type == 'received'){
