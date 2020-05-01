7cee40545cc477d9c014127b24dc8ce56c5373f8
ElderSam <eldersamuel98@gmail.com>
2020-05-01 13:58:15 -0300

    melhorando layout das mensagens

--- public/scripts.js
+++ public/scripts.js
@@ -3,8 +3,49 @@ var socket = io('http://localhost:3000'); //conecta com o Socket do backend, ouv
     
 var objDiv = document.querySelector(".messages");
 
-function renderMessage(message){
-    $('.messages').append('<div class="message"><strong>'+ message.author + '</strong>: '+ message.message + '</div>')
+function renderMessage(message, type){
+    //$('.messages').append(`<div class="message ${type}"><strong> ${message.author} </strong>: ${message.message} </div>`)
+
+    // Obtém a data/hora atual
+    var data = new Date();
+
+    // Guarda cada pedaço em uma variável
+    var dia     = data.getDate();           // 1-31
+    var dia_sem = data.getDay();            // 0-6 (zero=domingo)
+    var mes     = data.getMonth();          // 0-11 (zero=janeiro)
+    var ano2    = data.getYear();           // 2 dígitos
+    var ano4    = data.getFullYear();       // 4 dígitos
+    var hora    = data.getHours();          // 0-23
+    var min     = data.getMinutes();        // 0-59
+    var seg     = data.getSeconds();        // 0-59
+    var mseg    = data.getMilliseconds();   // 0-999
+    var tz      = data.getTimezoneOffset(); // em minutos
+
+    // Formata a data e a hora (note o mês + 1)
+    var str_data = dia + '/' + (mes+1) + '/' + ano4;
+    var str_hora = hora + ':' + min + ':' + seg;
+
+    if(type == 'received'){
+        align = ''
+        float = ''      
+        
+    }else{
+        align = 'align-right'
+        float = 'float-right'
+        message.author = 'eu'   
+    }
+
+    $('.messages').append(
+        `<li class="clearfix">
+            <div class="message-data ${align}">
+              <span class="message-data-time" >${str_hora}, Hoje</span> &nbsp; &nbsp;
+              <span class="message-data-name" ><strong>${message.author}</strong> </span> <i class="fa fa-circle me"></i>
+              
+            </div>
+            <div class="message ${float} ${type}">
+            ${message.message}
+            </div>
+    </li>`);
     
     objDiv.scrollTop = objDiv.scrollHeight; //arrasta o Scroll sempre para baixo, para mostrar as últimas mensagens
 
@@ -36,7 +77,7 @@ socket.on('previousMessages', function(messages){
 });*/
 
 socket.on('receivedMessage', function(message){
-    renderMessage(message);
+    renderMessage(message, 'received');
 });
 
 $('#chat').submit(function(event){ //quando eu enviar a mensagem
@@ -51,7 +92,7 @@ $('#chat').submit(function(event){ //quando eu enviar a mensagem
             message: message,
         };
 
-        renderMessage(messageObject) //mostra a mensagem na tela
+        renderMessage(messageObject, 'send') //mostra a mensagem na tela
         
         response = socket.emit('sendMessage', messageObject); //envia a mensagem
         if(!response.connected){
